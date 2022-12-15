1d7c7181f8b4862a2f9d393c79647bcf2a70c34e
unknown <18220004@std.stei.itb.ac.id>
2022-12-15 08:32:32 +0700

    fix HTML routing

--- app.py
+++ app.py
@@ -269,7 +269,7 @@ def login():
 def getotp():
     otp = random.randint(100000,999999) 
     session['otp'] = otp
-    return redirect(url_for('tubeststkelompok102/loginotp'))
+    return redirect(url_for('loginotp'))
 
 @app.route('/loginotp', methods=['GET', 'POST'])
 def loginotp():
